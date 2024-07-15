59c6b2d41c8687aeabe39d8ca1caa9d84ea464ab
IzakCancino <84246483+IzakCancino@users.noreply.github.com>
2024-07-14 19:59:59 -0500

    WPF tools, proprieties, and events

--- WPF/WPF/MainWindow.xaml.cs
+++ WPF/WPF/MainWindow.xaml.cs
@@ -19,6 +19,47 @@ namespace WPF
         public MainWindow()
         {
             InitializeComponent();
+
+            BrushConverter brushConverter = new BrushConverter();
+            Brush redColor = (Brush)brushConverter.ConvertFromString("#c55");
+            Brush greenColor = (Brush)brushConverter.ConvertFromString("#5c5");
+            Brush blueColor = (Brush)brushConverter.ConvertFromString("#55c");
+
+            Grid myGrid = new Grid();
+
+            Button myBtn = new Button();
+            WrapPanel myWrap = new WrapPanel();
+
+            TextBlock myTextBlock1 = new TextBlock();
+            myTextBlock1.Text = "I'm";
+            myTextBlock1.Foreground = redColor;
+            myWrap.Children.Add(myTextBlock1);
+
+            TextBlock myTextBlock2 = new TextBlock();
+            myTextBlock2.Text = "a colored";
+            myTextBlock2.Foreground = greenColor;
+            myWrap.Children.Add(myTextBlock2);
+
+            TextBlock myTextBlock3 = new TextBlock();
+            myTextBlock3.Text = "button!";
+            myTextBlock3.Foreground = blueColor;
+            myWrap.Children.Add(myTextBlock3);
+
+            myBtn.Content = myWrap;
+            myBtn.Width = 120;
+            myBtn.Height = 40;
+            myBtn.Background = Brushes.Orange;
+            myGrid.Children.Add(myBtn);
+
+            RootGrid.Children.Add(myGrid);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine(sender);
+            Console.WriteLine(e);
+
+            this.Background = Brushes.LightGray;
         }
     }
 }
